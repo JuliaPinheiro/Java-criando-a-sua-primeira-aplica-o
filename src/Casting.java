306dc84218888d788aee41bde28bfcb6295319bd
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-09 17:25:51 -0300

    feat: casting

--- src/Casting.java
+++ src/Casting.java
@@ -1,6 +1,11 @@
+//Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.
+
 public class Casting {
     public static void main(String[] args) {
 
+        double varDouble = (int)1.97;
+        int varInt = 1;
+        System.out.println(varDouble);
 
     }
 }
