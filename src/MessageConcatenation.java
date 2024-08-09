6690ecd3bfd3fc6e3916683b05015e422dfda2de
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-09 18:16:09 -0300

    feat: char

---
+++ src/MessageConcatenation.java
@@ -0,0 +1,9 @@
+//Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+
+public class MessageConcatenation {
+    public static void main(String[] args) {
+        char letra = 'a';
+        String palavra = "palavra";
+        System.out.println("essa mensagem foi feita para exibir a " + letra + "e a " + palavra);
+    }
+}
