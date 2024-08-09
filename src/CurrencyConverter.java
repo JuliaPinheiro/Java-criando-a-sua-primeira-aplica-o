6168b7ee3aa50b70a56420297ab77aafebf137fc
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-09 19:00:15 -0300

    feat: Currency converter

---
+++ src/CurrencyConverter.java
@@ -0,0 +1,12 @@
+/*
+* Declare uma variável do tipo double valorEmDolares. Atribua um valor em dólares a essa variável. Considere que o valor de 1 dólar é equivalente a 4.94 reais.
+* Realize a conversão do valor em dólares para reais e imprima o resultado formatado.
+* */
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        double valorEmDolares = 2.98* 4.94;
+        System.out.println("a conversão de dolar para real é de: " + valorEmDolares);
+
+    }
+}
