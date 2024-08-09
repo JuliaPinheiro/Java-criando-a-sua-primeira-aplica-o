6d47ad7da8863cab4e6ca3c845799c3ecc6aab2c
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-09 18:26:26 -0300

    feat: price

---
+++ src/TotalPriceCalculator.java
@@ -0,0 +1,14 @@
+/*
+* Declare uma variável do tipo double precoProduto e uma variável do tipo int (quantidade).
+* Calcule o valor total multiplicando o preço do produto pela quantidade e apresente o resultado em uma mensagem.
+* */
+public class TotalPriceCalculator {
+        public static void main(String[] args) {
+
+            double precoProduto = 2.99;
+            int quantidadeProduto = 50;
+
+            System.out.println("Valor total do produto: " + precoProduto*quantidadeProduto);
+
+        }
+}
