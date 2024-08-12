8d428742ff28e52f74412f42e8f1fbd0cd782422
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-12 08:15:54 -0300

    feat: repetition structure

---
+++ src/Loop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+        double MediaAvaliacao = 0;
+        double nota = 0;
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite sua avaliação para o filme: ");
+            nota = scan.nextDouble();
+            MediaAvaliacao += nota;
+        }
+
+        MediaAvaliacao = MediaAvaliacao / 3;
+        System.out.println("Media Avaliacao: " + MediaAvaliacao);
+
+    }
+}
